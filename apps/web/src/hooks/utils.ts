d0d35f89c3fe1eea5cdea963a635f5ef3902beda
Soufian <soufian@fantomshift.com>
2025-06-06 07:52:57 +0200

    fix: update LangGraph API URL to use relative path for client instantiation

--- apps/web/src/hooks/utils.ts
+++ apps/web/src/hooks/utils.ts
@@ -1,8 +1,7 @@
 import { Client } from "@langchain/langgraph-sdk";
-import { LANGGRAPH_API_URL } from "@/constants";
 
 export const createClient = () => {
   return new Client({
-    apiUrl: LANGGRAPH_API_URL,
+    apiUrl: "/api",
   });
 };
